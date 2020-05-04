3d6a7b465d03164a7d69619ccf4f4262182c8a33
松崎匡浩 <game.1andwork.1etc@gmail.com>
2020-05-04 11:20:17 +0900

    [add]finished adding the like function

--- app/models/book.rb
+++ app/models/book.rb
@@ -5,6 +5,7 @@ class Book < ApplicationRecord
 	#presence trueは空欄の場合を意味する。
 	validates :title, presence: true
 	validates :body, presence: true, length: {maximum: 200}
+	
 	def favorited_by?(user)
 		favorites.where(user_id: user.id).exists?
 	end
